2a9ed6fb9a9b05c70531fbcda172d91e074978f1
Dtop02 <156946626+Dtop02@users.noreply.github.com>
2024-02-14 10:37:58 -0700

    Create pandas_sandbox.py

---
+++ pandas_sandbox.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# list_teams = ['49ers', 'steelers','Cowboys']
+
+# print(type(list_teams))
+
+# print(list_teams)
+
+# sereis_teams = pd.Series(list_teams)
+
+df = pd.read_csv('big-mac-full-index.csv')
+
+print(type(df))
