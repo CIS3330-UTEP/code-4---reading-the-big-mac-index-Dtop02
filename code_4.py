ad53e84719cda9344a3c7a428fb16289e56fd45b
Dtop02 <156946626+Dtop02@users.noreply.github.com>
2024-02-16 17:14:16 -0700

    Updated Code

--- code_4.py
+++ code_4.py
@@ -1,18 +1,47 @@
-import csv
 import pandas as pd
-big_mac_file = './big-mac-full-index.csv'
 
-def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+def get_big_mac_price_by_year(year, country_code):
+    df = pd.read_csv('big-mac-full-index.csv')
+    fil_d = df[(df['iso_a3'].str.lower() == country_code.lower()) & (df['date'].str.startswith(str(year)))]
+    rounded_price = round(fil_d['dollar_price'].mean(), 2)
+    return rounded_price
+
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    df = pd.read_csv('big-mac-full-index.csv')
+    fil_d = df[df['iso_a3'].str.lower() == country_code.lower()]
+    mp = fil_d['dollar_price'].mean()
+    rounded_price = round(mp, 2)
+    return rounded_price
+
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.read_csv('big-mac-full-index.csv')
+    fil_d = df[df['date'].str.startswith(str(year))]
+    chep_row = fil_d.loc[fil_d['dollar_price'].idxmin()]
+    country_name = chep_row['name']
+    country_code = chep_row['iso_a3']
+    dol_pr = chep_row['dollar_price']
+    return f"{country_name}({country_code}): ${dol_pr:.1f}"
+
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.read_csv('big-mac-full-index.csv')
+    fil_d = df[df['date'].str.startswith(str(year))]
+    expen_row = fil_d.loc[fil_d['dollar_price'].idxmax()]
+    country_name = expen_row['name']
+    country_code = expen_row['iso_a3']
+    dol_pr = expen_row['dollar_price']
+    return f"{country_name}({country_code}): ${dol_pr:.1f}"
+
+
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    result_a = get_big_mac_price_by_year(2009,"mex")
+    print(result_a)
+    result_b = get_big_mac_price_by_country ("mex")
+    print(result_b)
+    result_c = get_the_cheapest_big_mac_price_by_year (2008)
+    print (result_c)
+    result_d = get_the_most_expensive_big_mac_price_by_year (2014)
+    print (result_d)
